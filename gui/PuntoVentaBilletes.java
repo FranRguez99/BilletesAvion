2d16cfd6929c72abc51e0ae406274ea4185547dc
FranRguez99 <92090696+FranRguez99@users.noreply.github.com>
2022-03-09 13:30:36 +0100

    Panel trayecto creado

--- gui/PuntoVentaBilletes.java
+++ gui/PuntoVentaBilletes.java
@@ -2,12 +2,12 @@ package gui;
 
 import javax.swing.*;
 import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class PuntoVentaBilletes {
-
-
-    public  void interfaz() {
+    
+    public void interfaz() {
         JFrame ventanaPrincipal = new JFrame("Air Carmela");
         ventanaPrincipal.setSize(700, 250);
         ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -31,13 +31,50 @@ public class PuntoVentaBilletes {
         pModalidad.setBorder(borde);
         pModalidad.setSize(700,500);
 
-        //Seccion Trayecto
+        // Sección Trayecto
         // Crea los paneles este
         JPanel pTrayecto=new JPanel();
+        JPanel pOrigen = new JPanel();
+        pOrigen.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JPanel pDestino = new JPanel();
+        pDestino.setLayout(new FlowLayout(FlowLayout.CENTER));
+        JPanel pPersonas = new JPanel();
+        pPersonas.setLayout(new FlowLayout(FlowLayout.CENTER));
 
         // Contenido de los paneles
+        // Panel origen
+        JLabel lOrigen = new JLabel("Origen: ");
+        JComboBox<String> cbOrigen = crearCaja();
+        cbOrigen.setSize(300, 300);
+        pOrigen.add(lOrigen);
+        pOrigen.add(cbOrigen);
 
-        //Seccion Punto de venta de billetes
+        // Panel destino
+        JLabel lDestino = new JLabel("Destino: ");
+        JComboBox<String> cbDestino = crearCaja();
+        pDestino.add(lDestino);
+        pDestino.add(cbDestino);
+
+        // Panel Personas
+        JLabel lPersonas = new JLabel("Nº Personas:");
+        JSpinner sPersonas = new JSpinner(new SpinnerNumberModel(1, 1, 10, 1));
+        JButton bPersonas = new JButton("Buscar");
+        pPersonas.add(lPersonas);
+        pPersonas.add(sPersonas);
+        pPersonas.add(bPersonas);
+
+        // Panel trayecto
+        Border trayectoInterior = BorderFactory.createEmptyBorder(0, 35, 0, 35);
+        Border trayectoExterior = BorderFactory.createTitledBorder(null, "Trayecto", TitledBorder.CENTER, TitledBorder.TOP);
+        Border bTrayecto = BorderFactory.createCompoundBorder(trayectoExterior, trayectoInterior);
+        pTrayecto.setBorder(bTrayecto);
+        pTrayecto.setLayout(new BorderLayout());
+        pTrayecto.add(pOrigen, BorderLayout.NORTH);
+        pTrayecto.add(pDestino, BorderLayout.CENTER);
+        pTrayecto.add(pPersonas, BorderLayout.SOUTH);
+
+
+        // Sección Punto de venta de billetes
         JLabel etiVentaBilletes=new JLabel("PUNTO DE VENTA DE BILLETES");
         pPuntoDeVenta.add(etiVentaBilletes);
 
@@ -46,7 +83,21 @@ public class PuntoVentaBilletes {
         ventanaPrincipal.add(pPuntoDeVenta,BorderLayout.NORTH);
         ventanaPrincipal.add(pDetalles,BorderLayout.WEST);
         ventanaPrincipal.add(pTrayecto,BorderLayout.EAST);
-        ventanaPrincipal.pack();
         ventanaPrincipal.setVisible(true);
+    }
+
+    private JComboBox<String> crearCaja() {
+        JComboBox<String> res = new JComboBox<>();
+        res.addItem("---------");
+        res.addItem("Sevilla");
+        res.addItem("Madrid");
+        res.addItem("Barcelona");
+        res.addItem("Valencia");
+        res.addItem("Bilbao");
+        res.addItem("Málaga");
+        res.addItem("A Coruña");
+        res.addItem("Santander");
+        res.addItem("Asturias");
+        return res;
     }
 }
