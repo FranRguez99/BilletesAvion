d381221c5fa95444234bc94bd6e9df053e4de38f
Aolivasuarez7 <92518747+Aolivasuarez7@users.noreply.github.com>
2022-03-09 13:49:13 +0100

    Add files via upload

--- gui/PuntoVentaBilletes.java
+++ gui/PuntoVentaBilletes.java
@@ -1,52 +1,101 @@
-package gui;
-
 import javax.swing.*;
-import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 
 public class PuntoVentaBilletes {
+ public  void interfaz() {
+  JFrame ventanaPrincipal = new JFrame("Adivina el resultado de la suma");
+  ventanaPrincipal.setSize(700, 250);
+  ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  ventanaPrincipal.setLayout(new BorderLayout());
+
+  //Crea los paneles
+  JPanel pPuntoDeVenta=new JPanel();
+  pPuntoDeVenta.setAlignmentX(0);
+  JPanel pDetalles=new JPanel();
+  JPanel pTrayecto=new JPanel();
+
+  GridLayout experimentLayout = new GridLayout(0,1);
+  pDetalles.setLayout(experimentLayout);
+
+  //Seccion Detalles
+  JPanel pModalidad=new JPanel();
+  JPanel pFechaIda=new JPanel();
+  JPanel pFechaVuelta=new JPanel();
+  pDetalles.add(pModalidad);
+  pDetalles.add(pFechaIda);
+  pDetalles.add(pFechaVuelta);
+
+     //Seccion Ida y Vuelta
+
+  JRadioButton opcion1=new JRadioButton("Ida Solo");
+  JRadioButton opcion2=new JRadioButton("Ida/Vuelta");
+  pModalidad.add(opcion1);
+  pModalidad.add(opcion2);
+  TitledBorder bordeModalidad =BorderFactory.createTitledBorder("Modalidad");
+  bordeModalidad.setTitleJustification(TitledBorder.CENTER);
+  pModalidad.setBorder(bordeModalidad);
+  pModalidad.setSize(15,15);
+
+    //Fecha ida
+  TitledBorder bordeMIda =BorderFactory.createTitledBorder("Fecha ida");
+  pFechaIda.setBorder(bordeMIda);
+  pFechaIda.setSize(15,15);
+   //Fecha vuelta
+  TitledBorder bordeVuelta =BorderFactory.createTitledBorder("Fecha vuelta");
+  bordeMIda.setTitleJustification(TitledBorder.CENTER);
+  pFechaVuelta.setBorder(bordeVuelta);
+  pFechaVuelta.setSize(15,15);
 
 
-    public  void interfaz() {
-        JFrame ventanaPrincipal = new JFrame("Air Carmela");
-        ventanaPrincipal.setSize(700, 250);
-        ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        ventanaPrincipal.setLayout(new BorderLayout());
 
-        //Seccion Detalles
-        //Crea los paneles oeste
-        JPanel pPuntoDeVenta=new JPanel();
-        pPuntoDeVenta.setAlignmentX(0);
-        JPanel pDetalles=new JPanel();
-        JPanel pModalidad=new JPanel();
-        JPanel pFechaIDa=new JPanel();
-        JPanel pFechaVuelta=new JPanel();
-        pDetalles.add(pModalidad);
+   JLabel DiaIda=new JLabel("Día");
+   JSpinner diasIda=new JSpinner();
+  JLabel mesIda=new JLabel("Mes");
+  JSpinner mesesIda=new JSpinner();
+  JLabel anioIda=new JLabel("Año");
+  JSpinner aniosIda=new JSpinner();
 
-        JRadioButton opcion1=new JRadioButton("Ida Solo");
-        JRadioButton opcion2=new JRadioButton("Ida/Vuelta");
-        pModalidad.add(opcion1);
-        pModalidad.add(opcion2);
-        Border borde=BorderFactory.createTitledBorder("Modalidad");
-        pModalidad.setBorder(borde);
-        pModalidad.setSize(700,500);
+  //añade elementos al panel Ida
+pFechaIda.add(DiaIda);
+  pFechaIda.add(diasIda);
+  pFechaIda.add(mesIda);
+  pFechaIda.add(mesesIda);
+  pFechaIda.add(anioIda);
+  pFechaIda.add(aniosIda);
 
-        //Seccion Trayecto
-        // Crea los paneles este
-        JPanel pTrayecto=new JPanel();
+ //** añade elementos al panel vuelta
 
-        // Contenido de los paneles
+  JLabel DiaV=new JLabel("Día");
+  JSpinner diasV=new JSpinner();
+  JLabel mesV=new JLabel("Mes");
+  JSpinner mesesV=new JSpinner();
+  JLabel anioV=new JLabel("Año");
+  JSpinner aniosV=new JSpinner();
 
-        //Seccion Punto de venta de billetes
-        JLabel etiVentaBilletes=new JLabel("PUNTO DE VENTA DE BILLETES");
-        pPuntoDeVenta.add(etiVentaBilletes);
+  pFechaVuelta.add(DiaV);
+  pFechaVuelta.add(diasV);
+  pFechaVuelta.add(mesV);
+  pFechaVuelta.add(mesesV);
+  pFechaVuelta.add(anioV);
+  pFechaVuelta.add(aniosV);
 
 
-        //Añade elementos a la ventana principal
-        ventanaPrincipal.add(pPuntoDeVenta,BorderLayout.NORTH);
-        ventanaPrincipal.add(pDetalles,BorderLayout.WEST);
-        ventanaPrincipal.add(pTrayecto,BorderLayout.EAST);
-        ventanaPrincipal.pack();
-        ventanaPrincipal.setVisible(true);
-    }
+
+
+
+  //Seccion Trayecto
+
+
+  //Seccion Punto de venta de billetes
+  JLabel etiVentaBilletes=new JLabel("PUNTO DE VENTA DE BILLETES");
+  pPuntoDeVenta.add(etiVentaBilletes);
+
+
+  //Añade elementos a la ventana principal
+  ventanaPrincipal.add(pPuntoDeVenta,BorderLayout.NORTH);
+  ventanaPrincipal.add(pDetalles,BorderLayout.WEST);
+  ventanaPrincipal.add(pTrayecto,BorderLayout.EAST);
+  ventanaPrincipal.setVisible(true);
+ }
 }
